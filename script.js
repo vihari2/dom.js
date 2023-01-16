1a168173582c333edfb42275633a20370b68d48d
vitoria <vitoria.dev.f@gmail.com>
2023-01-16 00:34:30 -0300

    [delete] prettier

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector("#fale");
-
-button.addEventListener("click", faleComigo);
-
-function faleComigo() {
-  const divContato = document.querySelector(".contato");
-
-  divContato.innerText = "Olá pessoa, deu certo!";
-}
